d2bce02710cd0dd9024d60c1254cae10c2ce5456
kelechi nnadi <kelechinnadi019@gmail.com>
2023-12-30 22:05:36 +0100

    the ode

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 # configure using puppet
 
-resources { 'config file':
-name    => 'config',
+file { '/home/ubuntu/.ssh/config':
+ensure  => present,
+mode    => '0600',
+owner   => 'ubuntu',
+}
 
+# Define the template for SSH client configuration
+file { '/etc/puppetlabs/code/environments/production/modules/your_module/templates/ssh_config.erb':
+ensure  => present,
+mode    => '0644',
+content => "# SSH client configuration\n\n54.145.157.0\nIdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
 }
