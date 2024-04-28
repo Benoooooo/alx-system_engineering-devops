d49c5939e60fb062a16276385d665aa1734fc665
kelechi nnadi <kelechinnadi019@gmail.com>
2024-04-28 01:18:22 +0100

    the code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-''' queries the Reddit API and returns the number of subscribers'''
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    '''Get the number of subscribers'''
-    headers = {'user-Agent': 'MyBot/1.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers)
-
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyRedditClient/1.0"}  # Set a custom User-Agent header
+    
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data["data"]["subscribers"]
         return subscribers
-    elif response.status_code == 404:
-        return 0
     else:
-        print(f"Error:{response.status_code}")
-        return 0
+        return 0  # Invalid subreddit or error
